7a194e4a7781e16681365b2386cdf3b6cbe8da10
Carlos Silva <csilva2810@gmail.com>
2017-01-30 13:43:44 -0200

    Removing history from repository

---
+++ app/src/main/java/csilva2810/udacity/com/popularmovies/constants/MoviesApi.java
@@ -0,0 +1,22 @@
+package csilva2810.udacity.com.popularmovies.constants;
+
+/**
+ * Created by carlinhos on 1/12/17.
+ */
+public class MoviesApi {
+
+        public static final String SCHEME = "https";
+        public static final String AUTHORITY = "api.themoviedb.org/3";
+
+        public static final String REVIEWS_PATH = "reviews";
+        public static final String VIDEOS_PATH = "videos";
+
+        public static final String IMAGE_POSTER_SMALL = "w185/";
+        public static final String IMAGE_POSTER_MEDIUM = "w342/";
+        public static final String IMAGE_POSTER_LARGE = "w500/";
+        public static final String IMAGE_BACKDROP_SMALL = "w300/";
+        public static final String IMAGE_BACKDROP_MEDIUM = "w780/";
+        public static final String IMAGE_BACKDROP_LARGE = "w1280/";
+        public static final String IMAGES_URL = "http://image.tmdb.org/t/p/";
+
+}
